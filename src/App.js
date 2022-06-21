654540bb249c72cb1082a3c26f85f9a8575a88d9
Zack Lian <zlian@lifetime.com>
2022-06-21 01:47:57 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef } from "react";
+import { Editor } from "@tinymce/tinymce-react";
 
-function App() {
+export default function App() {
+  const editorRef = useRef(null);
+  const log = () => {
+    if (editorRef.current) {
+      console.log(editorRef.current.getContent());
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Editor
+        onInit={(evt, editor) => (editorRef.current = editor)}
+        initialValue=""
+        init={{
+          height: 500,
+          menubar: false,
+          plugins: [
+            "advlist autolink lists link image charmap print preview anchor",
+            "searchreplace visualblocks code fullscreen",
+            "insertdatetime media table paste code wordcount",
+          ],
+          toolbar:
+            "undo redo | formatselect | " +
+            "bold italic backcolor | alignleft aligncenter " +
+            "alignright alignjustify | bullist numlist outdent indent | " +
+            "removeformat remove | metadata ",
+          content_style:
+            "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
+          setup: function (editor) {
+            editor.ui.registry.addMenuButton("metadata", {
+              tooltip: "Insert Attribute",
+              icon: "comment-add",
+              fetch: function (callback) {
+                var items = [
+                  {
+                    type: "menuitem",
+                    text: "Name",
+                    onAction: function () {
+                      editor.insertContent(
+                        '<span contenteditable="false" style="color: black; font-weight: bold; background-color: #e7f0f7" class="placeholder ui-draggable" unselectable="ON">[Person Name]</span>'
+                      );
+                    },
+                  },
+                  {
+                    type: "menuitem",
+                    text: "Email",
+                    onAction: function () {
+                      editor.insertContent(
+                        '<span contenteditable="false" style="color: black; font-weight: bold; background-color: #e7f0f7" class="placeholder ui-draggable" unselectable="ON">[Person Email]</span>'
+                      );
+                    },
+                  },
+                ];
+                callback(items);
+              },
+            });
+          },
+        }}
+      />
+      <button onClick={log}>Log editor content</button>
+    </>
   );
 }
-
-export default App;
